aa661323bc90b34105d1ffa983df086a20dc77c3
Tomek Marciniak <tmarciniakm@gmail.com>
2024-03-26 21:24:56 +0100

    chore(nav): remove contact

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,40 +1,78 @@
-import NextImage from 'next/image'
-import NextLink from 'next/link'
-import { Button } from '@/components/ui/button'
-import { PhoneCallIcon } from 'lucide-react'
+import NextImage from "next/image";
+import NextLink from "next/link";
+import { Button } from "@/components/ui/button";
+import { PhoneCallIcon, DribbbleIcon, LayersIcon } from "lucide-react";
 
-const EMAIL = 'team@palladians.xyz'
-const CAL_URL = 'https://cal.com/palladians'
-const X_URL = 'https://x.com/palladians_xyz'
-const DISCORD_URL = 'https://discord.gg/ExzzfTGUnB'
-const LINKEDIN_URL = 'https://linkedin.com/company/palladians'
+const EMAIL = "team@palladians.xyz";
+const CAL_URL = "https://cal.com/palladians";
+const X_URL = "https://x.com/palladians_xyz";
+const DISCORD_URL = "https://discord.gg/ExzzfTGUnB";
+const LINKEDIN_URL = "https://linkedin.com/company/palladians";
+const DRIBBBLE_URL = "https://dribbble.com/palladians";
+const LAYERS_URL = "https://layers.to/palladians";
 
 export const Footer = () => {
   return (
-    <footer className="flex flex-col gap-16 lg:gap-32 py-24 bg-zinc-900 relative">
-      <NextImage src="/oval-blur.png" width={1200} height={500} className="hidden lg:flex absolute top-0 left-0 right-0 w-full h-full rotate-180" alt="Blurry Background" />
-      <div className="container grid grid-cols-1 lg:grid-cols-5 gap-16 relative">
+    <footer className="relative flex flex-col gap-16 bg-zinc-900 py-24 lg:gap-32">
+      <NextImage
+        src="/oval-blur.png"
+        width={1200}
+        height={500}
+        className="absolute left-0 right-0 top-0 hidden h-full w-full rotate-180 lg:flex"
+        alt="Blurry Background"
+      />
+      <div className="container relative grid grid-cols-1 gap-16 lg:grid-cols-5">
         <div>
           <NextLink href="/">
-            <NextImage src="/logo-symbol.svg" width={50} height={50} alt="Logo Symbol" />
+            <NextImage
+              src="/logo-symbol.svg"
+              width={50}
+              height={50}
+              alt="Logo Symbol"
+            />
           </NextLink>
         </div>
         <div className="flex flex-col gap-4">
           <h4 className="text-xl">Social Media</h4>
-          <div className="flex gap-2 items-center">
+          <div className="flex items-center gap-2">
             <Button variant="secondary" size="icon" asChild>
               <a href={X_URL} target="_blank" rel="noopener noreferrer">
-                <NextImage src="/twitter-x.svg" width={20} height={20} alt="x.com" />
+                <NextImage
+                  src="/twitter-x.svg"
+                  width={20}
+                  height={20}
+                  alt="x.com"
+                />
               </a>
             </Button>
             <Button variant="secondary" size="icon" asChild>
               <a href={DISCORD_URL} target="_blank" rel="noopener noreferrer">
-                <NextImage src="/discord.svg" width={20} height={20} alt="Discord" />
+                <NextImage
+                  src="/discord.svg"
+                  width={20}
+                  height={20}
+                  alt="Discord"
+                />
               </a>
             </Button>
             <Button variant="secondary" size="icon">
               <a href={LINKEDIN_URL} target="_blank" rel="noopener noreferrer">
-                <NextImage src="/linkedin.svg" width={20} height={20} alt="LinkedIn" />
+                <NextImage
+                  src="/linkedin.svg"
+                  width={20}
+                  height={20}
+                  alt="LinkedIn"
+                />
+              </a>
+            </Button>
+            <Button variant="secondary" size="icon">
+              <a href={DRIBBBLE_URL} target="_blank" rel="noopener noreferrer">
+                <DribbbleIcon />
+              </a>
+            </Button>
+            <Button variant="secondary" size="icon">
+              <a href={LAYERS_URL} target="_blank" rel="noopener noreferrer">
+                <LayersIcon />
               </a>
             </Button>
           </div>
@@ -42,11 +80,19 @@ export const Footer = () => {
         <div className="flex flex-col gap-4">
           <h4 className="text-xl">Contact</h4>
           <div className="flex flex-col gap-4">
-            <a href={`mailto:${EMAIL}`} className="flex gap-2 items-center text-muted-foreground">
+            <a
+              href={`mailto:${EMAIL}`}
+              className="text-muted-foreground flex items-center gap-2"
+            >
               <NextImage src="/email.png" width={24} height={20} alt="Email" />
               <span>{EMAIL}</span>
             </a>
-            <a href={CAL_URL} target="_blank" rel="noopener noreferrer" className="flex gap-2 items-center text-muted-foreground">
+            <a
+              href={CAL_URL}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-muted-foreground flex items-center gap-2"
+            >
               <PhoneCallIcon className="text-primary" />
               <span>Book Call</span>
             </a>
@@ -54,26 +100,53 @@ export const Footer = () => {
         </div>
         <div className="flex flex-col gap-4">
           <h4 className="text-xl">Collective</h4>
-          <div className="flex flex-col gap-4 mt-2">
-            <a href="/#about" className="text-muted-foreground">About</a>
-            <a href="/#products" className="text-muted-foreground">Products</a>
-            <NextLink href="/blog" className="text-muted-foreground">Blog</NextLink>
+          <div className="mt-2 flex flex-col gap-4">
+            <a href="/#about" className="text-muted-foreground">
+              About
+            </a>
+            <a href="/#products" className="text-muted-foreground">
+              Products
+            </a>
+            <NextLink href="/blog" className="text-muted-foreground">
+              Blog
+            </NextLink>
           </div>
         </div>
         <div className="flex flex-col gap-4">
           <h4 className="text-xl">Products</h4>
-          <div className="flex flex-col gap-4 mt-2">
-            <a href="https://pallad.xyz" target="_blank" rel="noopener noreferrer" className="text-muted-foreground">Pallad</a>
-            <a href="https://minaverse.xyz" target="_blank" rel="noopener noreferrer" className="text-muted-foreground">Minaverse</a>
-            <a href="https://github.com/palladians/herald" target="_blank" rel="noopener noreferrer" className="text-muted-foreground">Herald</a>
+          <div className="mt-2 flex flex-col gap-4">
+            <a
+              href="https://pallad.xyz"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-muted-foreground"
+            >
+              Pallad
+            </a>
+            <a
+              href="https://minaverse.xyz"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-muted-foreground"
+            >
+              Minaverse
+            </a>
+            <a
+              href="https://github.com/palladians/herald"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-muted-foreground"
+            >
+              Herald
+            </a>
           </div>
         </div>
       </div>
-      <div className="container flex flex-col lg:flex-row justify-center gap-8 relative">
+      <div className="container relative flex flex-col justify-center gap-8 lg:flex-row">
         <p>© 2023 Palladians</p>
         <NextLink href="/privacy">Privacy Policy</NextLink>
         <NextLink href="/terms">Terms of Service</NextLink>
       </div>
     </footer>
-  )
-}
+  );
+};
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,52 +1,80 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
-import NextImage from 'next/image'
-import NextLink from 'next/link'
-import { Button } from '@/components/ui/button'
-import { AlignRightIcon, XIcon } from 'lucide-react'
+import { useState } from "react";
+import NextImage from "next/image";
+import NextLink from "next/link";
+import { Button } from "@/components/ui/button";
+import { AlignRightIcon, XIcon } from "lucide-react";
 
-const Buttons = ({ mobile, callback }: { mobile?: boolean; callback?: () => void }) => {
+const Buttons = ({
+  mobile,
+  callback,
+}: {
+  mobile?: boolean;
+  callback?: () => void;
+}) => {
   return (
     <>
-      <Button variant={mobile ? 'ghost' : 'secondary'} onClick={callback} asChild>
+      <Button
+        variant={mobile ? "ghost" : "secondary"}
+        onClick={callback}
+        asChild
+      >
         <a href="/#about">About</a>
       </Button>
-      <Button variant={mobile ? 'ghost' : 'secondary'} onClick={callback} asChild>
+      <Button
+        variant={mobile ? "ghost" : "secondary"}
+        onClick={callback}
+        asChild
+      >
         <a href="/#products">Products</a>
       </Button>
-      <Button variant={mobile ? 'ghost' : 'secondary'} onClick={callback} asChild>
+      <Button
+        variant={mobile ? "ghost" : "secondary"}
+        onClick={callback}
+        asChild
+      >
         <NextLink href="/blog">Blog</NextLink>
       </Button>
       <Button onClick={callback} asChild>
-        <NextLink href="/contact">
-          Contact
-        </NextLink>
+        <NextLink href="mailto:team@palladians.xyz">Contact</NextLink>
       </Button>
     </>
-  )
-}
+  );
+};
 
 export const Navbar = () => {
-  const [mobileExpanded, setMobileExpanded] = useState(false)
+  const [mobileExpanded, setMobileExpanded] = useState(false);
   return (
-    <nav className="bg-background py-4 sticky top-0 left-0 right-0 z-10">
-      <div className="container flex justify-between items-center">
+    <nav className="bg-background sticky left-0 right-0 top-0 z-10 py-4">
+      <div className="container flex items-center justify-between">
         <NextLink href="/" onClick={() => setMobileExpanded(false)}>
-          <NextImage src="/logo.svg" width={200} height={64} alt="logo" className='w-[10rem] lg:w-[12rem]' />
+          <NextImage
+            src="/logo.svg"
+            width={200}
+            height={64}
+            alt="logo"
+            className="w-[10rem] lg:w-[12rem]"
+          />
         </NextLink>
         <div className="hidden items-center gap-4 lg:flex">
           <Buttons />
         </div>
         <div className="flex lg:hidden">
-          <Button size="icon" variant="ghost" onClick={() => setMobileExpanded(!mobileExpanded)}>
+          <Button
+            size="icon"
+            variant="ghost"
+            onClick={() => setMobileExpanded(!mobileExpanded)}
+          >
             {mobileExpanded ? <XIcon /> : <AlignRightIcon />}
           </Button>
         </div>
       </div>
-      {mobileExpanded && <div className="flex flex-col items-center gap-4 p-4">
-        <Buttons callback={() => setMobileExpanded(false)} mobile />
-      </div>}
+      {mobileExpanded && (
+        <div className="flex flex-col items-center gap-4 p-4">
+          <Buttons callback={() => setMobileExpanded(false)} mobile />
+        </div>
+      )}
     </nav>
-  )
-}
+  );
+};
