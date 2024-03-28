e38d8e2eb56f22dcf5d3d5e429ab0a1fd8601453
Tomek Marciniak <tmarciniakm@gmail.com>
2024-03-28 17:37:37 +0100

    chore(home): update proof

--- src/components/social-proof.tsx
+++ src/components/social-proof.tsx
@@ -4,7 +4,7 @@ export const SocialProof = () => {
   return (
     <section className="container flex flex-col items-start justify-center gap-16 lg:items-center">
       <div className="align-left lg:align-center text-lg text-muted-foreground">
-        We work with top brands such as
+        We&apos;ve worked with top brands and protocols
       </div>
       <div className="flex flex-col items-start justify-center gap-24 lg:flex-row lg:items-center">
         <a
@@ -14,8 +14,12 @@ export const SocialProof = () => {
         >
           <NextImage src="/obscura.svg" width={240} height={80} alt="Obscura" />
         </a>
-        <a href="https://iohk.io/" target="_blank" rel="noopener noreferrer">
-          <NextImage src="/iohk.svg" width={240} height={80} alt="IOHK" />
+        <a
+          href="https://cardano.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <NextImage src="/cardano.svg" width={240} height={80} alt="Cardano" />
         </a>
         <a
           href="https://minaprotocol.com/"
