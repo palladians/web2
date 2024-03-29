632d248dda270ce13ae5afcb75c053fe027b5b36
Tomek Marciniak <tmarciniakm@gmail.com>
2024-03-29 13:08:37 +0100

    chore(home): adjust sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,34 +1,25 @@
-import { getBlogPosts } from '@/api/getBlogPosts'
-import { getDynamicPages } from '@/api/getDynamicPages'
-import { MetadataRoute } from 'next'
+import { getDynamicPages } from "@/api/getDynamicPages";
+import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const blogPosts = (await getBlogPosts({ limit: 100 })).map((post) => ({
-    url: `https://palladians.xyz/blog/${post.slug}`,
-    lastModified: new Date(),
-    changeFrequency: 'weekly' as never,
-    priority: 0.8,
-  }))
-  const dynamicPages = (await getDynamicPages()).map((page) => ({
-    url: `https://palladians.xyz/${page.slug}`,
-    lastModified: new Date(),
-    changeFrequency: 'weekly' as never,
-    priority: 0.8,
-  }))
+  // const blogPosts = (await getBlogPosts({ limit: 100 })).map((post) => ({
+  //   url: `https://palladians.xyz/blog/${post.slug}`,
+  //   lastModified: new Date(),
+  //   changeFrequency: 'weekly' as never,
+  //   priority: 0.8,
+  // }))
+  // const dynamicPages = (await getDynamicPages()).map((page) => ({
+  //   url: `https://palladians.xyz/${page.slug}`,
+  //   lastModified: new Date(),
+  //   changeFrequency: "weekly" as never,
+  //   priority: 0.8,
+  // }));
   return [
     {
-      url: 'https://palladians.xyz',
+      url: "https://palladians.xyz",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 1,
     },
-    {
-      url: 'https://palladians.xyz/blog',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
-    ...blogPosts,
-    ...dynamicPages
-  ]
+  ];
 }
